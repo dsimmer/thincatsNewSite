dd1b1ee9fa2454f3a1b73162ec15eaaff0ee50b7
Dylan Simmer <goldenoblivion@gmail.com>
2020-02-25 11:57:32 +1100

    Use secure.thincats.com.au

--- wp-content/themes/thincats/js/customScripting.js
+++ wp-content/themes/thincats/js/customScripting.js
@@ -101,7 +101,7 @@ $(document).ready(function() {
  *
  */
 function registrationForm() {
-	if(window.location.pathname === "https://thincats.azurewebsites.net/account/lender/register/"){
+	if(window.location.pathname === "https://secure.thincats.com.au/account/lender/register/"){
 
 		jQuery.validator.addMethod(
 			"hasOnlyLowerCase",
